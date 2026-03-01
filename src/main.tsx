50a39cafa34a5831e5b4ed19f39bce6ec7d4cf60
Divit <divit26j@gmail.com>
2026-03-01 10:23:17 +0000

    rolled back to wokring

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from './App';
+import Dashboard from './components/Dashboard';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/editor" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
